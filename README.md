cdd59b60815d0e5a06831e8a3424cc3415717765
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-03-27 13:35:28 +0000

    Update README and setup description

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-# Ledger Python API
+# Fetch.AI Ledger Python API
 
-Python library code for interacting with the ledger
+Python SDK library used for interacting the Fetch.AI smart ledger. 
+
+### Features
+
+* TokenAPI - Send and receive tokens on the network as well as querying balances etc
+* TransactionAPI - Determine the status of sent transactions
+* Cryptography support
 
 ## Installation
 
-    $ python3 setup.py install
+The library can be installed directly from pip using the following command.
+
+    $ pip3 install -U fetchai-ledger-api
+    
+Note: Since the ledger is going through a period of rapid development we encourage developers to use the latest version
+of the library.
 
 ## Developing with the library
 
--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
+from os import path
 from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='fetchai-ledger-api',
-    version='0.1.3',
+    version='0.1.4',
     description='Tools and utilities for interacting with the ledger on the fetch network',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/fetchai/ledger-api-py',
     author='Edward FitzGerald',
     author_email='edward.fitzgerald@fetch.ai',
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'scripts']),
     install_requires=['requests', 'ecdsa'],
     extras_require={
