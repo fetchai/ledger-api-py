0b92886f6aaeae5ae299001a0f2e8598f64c5a48
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-09-02 10:14:00 +0100

    Update version to v0.8.0a1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fetchai-ledger-api',
-    version='0.0.0',
+    version='0.8.0a1',
     description='Tools and utilities for interacting with the ledger on the fetch network',
     url='https://github.com/fetchai/ledger-api-py',
     author='Edward FitzGerald',
