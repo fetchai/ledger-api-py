c42b201aa7e42570d9e41247bdf93f549fb840ce
Khan Baykaner <khan.baykaner@fetch.ai>
2019-09-16 14:15:09 +0100

    updated to include scaler

--- examples/crypto_prediction_model.py
+++ examples/crypto_prediction_model.py
@@ -25,6 +25,7 @@ from fetchai.ledger.crypto import Entity, Address
 GRAPH_FILE_NAME = "/Users/khan/fetch/models/crypto_price_prediction/bitcoin_price_prediction_graph.bin"
 
 EXAMPLE_INPUT_HISTORICS = "430.573, 428.26, 428.26, 428.26, 428.26, 428.26, 428.26, 428.26, 428.26, 428.26, 428.26, 428.26, 428.26, 428.26, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 430.804, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 433.01, 433.01, 433.01, 433.01, 432.14, 432.14, 432.14, 432.14, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 433.01, 430.9, 430.9, 430.9, 430.9, 430.9, 430.9, 430.9, 430.9, 430.9, 430.9, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 431.76, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 431.76, 431.76, 431.76, 431.75, 431.75, 431.75, 431.75, 431.75, 431.75, 431.75, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 429.12, 431, 431, 431, 431, 431, 431, 431, 431, 431, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 432.62, 428.27, 428.27, 428.27, 428.27, 428.27, 428.27, 428.27, 428.27, 428.27, 428.27, 428.27, 434.07, 434.07, 428.5, 434.07, 434.07, 434.07, 434.07, 434.07, 434.07, 434.07, 434.07, 434.07, 434.07, 434.07, 434.07, 428.6, 428.6, 428.6, 428.6, 428.6, 428.6, 428.6, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07, 432.07"
+EXAMPLE_LABEL_HISTORICS = "10328.3, 10320.0, 10320.0, 10322.0, 10325.0, 10325.0, 10325.0, 10325.8, 10325.8, 10326.6, 10326.6, 10326.9, 10326.9, 10321.0, 10321.0, 10321.1, 10315.3, 10315.2, 10315.2, 10315.0, 10315.0, 10316.8, 10319.2, 10319.2, 10319.2, 10321.1, 10322.6, 10335.0, 10339.6, 10345.0, 10345.8, 10341.9"
 
 CONTRACT_TEXT = """
 
@@ -33,6 +34,7 @@ persistent dataloader_state : DataLoader;
 persistent optimiser_state : Optimiser;
 persistent historics_state : Tensor;
 persistent prediction_state : Tensor;
+persistent scaler_state : Scaler;
 
 // Smart contract initialisation sets up our graph, dataloader, and optimiser
 // in this example we hard-code some values such as the expected input data size
@@ -45,6 +47,7 @@ function setup(owner : Address)
   use dataloader_state;
   use optimiser_state;
   use historics_state;
+  use scaler_state;
 
   var owner_balance = State<UInt64>(owner);
   owner_balance.set(1000000u64);
@@ -70,10 +73,15 @@ function setup(owner : Address)
   var historics = historics_state.get(Tensor(tensor_shape));
   historics_state.set(historics);
   
+  // initial scaler setup
+  var scaler = Scaler();
+  scaler_state.set(scaler);
+  
 endfunction
 
 // Method initial graph setup (we could forgo adding ops/layers
 // if the graph would later be set via a call to updateGraph)
+@query
 function graphSetup(g : Graph)
     // set up a trivial graph
     g.addPlaceholder("Input");
@@ -82,27 +90,44 @@ function graphSetup(g : Graph)
     g.addMeanSquareErrorLoss("Error", "Output", "Label");
 endfunction
 
+// Method to update scaler settings
+@action
+function setScale(min_val : UInt64, max_val : UInt64)
+    use scaler_state;
+    var scaler = scaler_state.get();
+    scaler.setScale(toFixed64(min_val), toFixed64(max_val));
+    scaler_state.set(scaler);
+endfunction
+
 // Method to set new historics as data changes
 @action
 function setHistorics(new_historics: String)
     use graph_state;
     use historics_state;
     use prediction_state;
+    use scaler_state;
 
     // set new historics
     var historics = historics_state.get();
     historics.fromString(new_historics);
 
-    // make new prediction
+    // normalise the historics
+    var scaler = scaler_state.get();
+    var scaled_historics = scaler.normalise(historics);
+    
+    // make prediction with normalised historics
     var g = graph_state.get();
-    g.setInput("Input", historics);
+    g.setInput("Input", scaled_historics);
     var prediction = g.evaluate("Output");
     var squeezed_pred = prediction.squeeze();
+    
+    // descale the predictions
+    var descaled_pred = scaler.deNormalise(squeezed_pred);
 
     // set new historics and graph states
     historics_state.set(historics);
     graph_state.set(g);
-    prediction_state.set(squeezed_pred);
+    prediction_state.set(descaled_pred);
 endfunction
 
 // method for querying currently set historics
@@ -152,7 +177,9 @@ function train(train_labels_string: String)
     var dataloader = dataloader_state.get();
     
     // add the historics as training data, and add provided labels
-    var train_labels : Tensor;
+    var tensor_shape = Array<UInt64>(1);
+    tensor_shape[0] = 1u64;
+    var train_labels = Tensor(tensor_shape);
     train_labels.fromString(train_labels_string);
     dataloader.addData(historics, train_labels);
      
@@ -201,8 +228,12 @@ def main():
 
     print("finished updating smart contract graph")
 
-    # set one real example input data set
     fet_tx_fee = 100000000
+
+    # set the scale of values to normalise to
+    api.sync(contract.action(api, 'setScale', fet_tx_fee, [entity1], 0, 15000))
+
+    # set one real example input data set
     api.sync(contract.action(api, 'setHistorics', fet_tx_fee, [entity1], EXAMPLE_INPUT_HISTORICS))
 
     current_historics = contract.query(api, 'getHistorics')
@@ -211,6 +242,9 @@ def main():
     # make a prediction
     current_prediction = contract.query(api, 'makePrediction')
     print("current prediction: " + current_prediction)
+
+    # check training
+    # api.sync(contract.action(api, 'train', fet_tx_fee, [entity1], EXAMPLE_LABEL_HISTORICS))
 
 if __name__ == '__main__':
     main()
