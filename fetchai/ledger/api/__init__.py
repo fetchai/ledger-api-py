74db83495cbe04ec9dde71acca7ee22a8fdadb93
Katharine Murphy <katharine.murphy@fetch.ai>
2019-09-16 15:23:15 +0100

    Adding docstrings to __init__.py of ledger/api

--- fetchai/ledger/api/__init__.py
+++ fetchai/ledger/api/__init__.py
@@ -37,15 +37,30 @@ def _iterable(value):
 
 
 class LedgerApi:
+    """This class encapsulates the top level functionality of the Ledger API.
+    """
     def __init__(self, host, port):
+        """Constructor method that creates the specific APIs.
+
+        :param host: the Ledger API url.
+        :param port: the Ledger API port.
+        :param tokens: an object reference to the TokenApi running on host and port.
+        :param contracts: an object reference to the ContractsApi running on host and port.
+        :param tx: an object reference to the TransactionApi running on host and port.
+        :param synergetic: an object reference to the SynergeticApi running on host and port.
+        """
         self.tokens = TokenApi(host, port)
         self.contracts = ContractsApi(host, port)
         self.tx = TransactionApi(host, port)
         self.synergetic = SynergeticApi(host, port)
 
     def sync(self, txs: Transactions):
+        """This function ensures all asynchronous transactions sent via a contract function are verified before code moves forward.
 
-        # given the inputs make sure that we correctly for the input set of values
+        :param txs: the transactions sent to the contract function.
+        """
+
+        # ensure inputs to sync are dealt with correctly
         if isinstance(txs, str):
             remaining = {txs}
         elif _iterable(txs):
@@ -53,11 +68,12 @@ class LedgerApi:
         else:
             raise TypeError('Unknown argument type')
 
+        # give a Transaction 2 minutes to verify
         limit = timedelta(minutes=2)
         start = datetime.now()
         while True:
 
-            # loop through all the remaining digests and poll them creating a set of completed in this round
+            # loop through all the remaining Transaction digests and poll them to see if they are completed.
             remaining -= set([digest for digest in remaining if self._poll(digest)])
 
             # once we have completed all the outstanding transactions
@@ -72,4 +88,9 @@ class LedgerApi:
             time.sleep(1)
 
     def _poll(self, digest):
+        """This function checks the Transaction digest state. All contract transactions should complete and return an "Executed" state.
+        However, Synergetic contract transactions with submit data should complete and return with a "Submitted" state.
+
+        :param digest: the transaction digests sent to sync.
+        """
         return self.tx.status(digest) in ('Executed', 'Submitted')
