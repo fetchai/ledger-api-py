258033ef68379aec0bf0711ac64cf55623c6d37a
Nelis Drost <cornelis.drost@fetch.ai>
2019-10-29 17:05:35 +0000

    Remove unused contract text from tests

--- tests/parser/test_shard_mask_parsing.py
+++ tests/parser/test_shard_mask_parsing.py
@@ -4,39 +4,6 @@ from lark import ParseError
 
 from fetchai.ledger.parser.etch_parser import EtchParser, UseWildcardShardMask, UnparsableAddress
 
-CONTRACT_TEXT = """
-persistent sharded balance_state : UInt64;
-persistent owner_name : String;
-
-@init
-function setup(owner : Address)
-  use balance_state[owner];
-  balance_state.set(owner, 1000000u64);
-endfunction
-
-@action
-function transfer(from: Address, to: Address, amount: UInt64)
-
-  use balance_state[from, to];
-
-  // Check if the sender has enough balance_state to proceed
-  if (balance_state.get(from) >= amount)
-
-    // update the account balances
-    balance_state.set(from, balance_state.get(from) - amount);
-    balance_state.set(to, balance_state.get(to, 0u64) + amount);
-  endif
-
-endfunction
-
-@query
-function balance(address: Address) : UInt64
-    use balance_state[address];
-    return balance_state.get(address, 0u64);
-endfunction
-
-"""
-
 # For testing use statements outside annotated functions
 # @init-setup() calls set_balance(), which uses a global outside an annotated function
 # @action-transfer() uses globals directly, and calls the sub(), which doesn't use globals
