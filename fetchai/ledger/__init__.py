3ee598cc9fd23d67db078372765de925fa75afe5
Nelis Drost <cornelis.drost@fetch.ai>
2019-12-12 15:06:33 +0000

    Update version to v0.11.0-alpha4

--- fetchai/ledger/__init__.py
+++ fetchai/ledger/__init__.py
@@ -1,5 +1,5 @@
 # Ledger version that this API is compatible with
-__version__ = '0.11.0-a3'
+__version__ = '0.11.0-a4'
 # This API is compatible with ledgers that meet all the requirements listed here:
 __compatible__ = ['>=0.11.0-alpha.7']
 
